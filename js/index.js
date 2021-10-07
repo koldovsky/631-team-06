2271cbe9ca0e4bff56a251cd0ce78506424f43e8
Mark333s <markomelnychyn333@gmail.com>
2021-10-07 14:14:12 +0000

    added js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-setInterval(function () {
-    const clockContainer = document.querySelector('.clock');
-    clockContainer.innerText = new Date().toLocaleTimeString();
-}, 1000);
+
 async function convertCurrency(){
     const convertTo=document.querySelector('.currency').value;
   const currencies = await getJson('https://api.exchangerate-api.com/v4/latest/USD');
   const rate= currencies.rates[converTo];
 document.querySelector('.convert')
- .addEventListener('click',convertCurrency)
+ .addEventListener('click',convertCurrency)}
+ setInterval(function () {
+    const clockContainer = document.querySelector('.clock');
+    clockContainer.innerText = new Date().toLocaleTimeString();
+}, 1000);
